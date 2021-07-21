9f6dcd241d2ef4e5bf8fa509af3b4a7df52f5076
Nikita Gamolsky <nikita.gamolsky@gmail.com>
2021-07-21 14:52:57 -0700

    start working on routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,46 @@
 import * as React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import firebase from "firebase/app";
 import "firebase/auth";
+import { ChakraProvider, Box, theme } from "@chakra-ui/react";
 import {
-  ChakraProvider,
-  Box,
-  VStack,
-  Grid,
-  theme,
-  Button,
-} from "@chakra-ui/react";
-import { ColorModeSwitcher } from "./components/ColorModeSwitcher";
-import { FirebaseAuthProvider } from "@react-firebase/auth";
+  FirebaseAuthProvider,
+  IfFirebaseAuthed,
+  IfFirebaseUnAuthed,
+} from "@react-firebase/auth";
 import { firebaseConfig } from "./config/firebase";
-import { Form, Formik } from "formik";
-import { InputField } from "./components/InputField";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <FirebaseAuthProvider firebase={firebase} {...firebaseConfig}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8} w={800} mx="auto">
-            <Formik
-              initialValues={{ username: "", password: "" }}
-              onSubmit={(values, { setSubmitting }) => {
-                setTimeout(() => {
-                  alert(JSON.stringify(values, null, 2));
-                  setSubmitting(false);
-                }, 400);
-              }}
-            >
-              {({ isSubmitting }) => (
-                <Form style={{ width: "90%" }}>
-                  <InputField
-                    label="Username"
-                    name="username"
-                    placeholder="Username"
-                  />
-                  <InputField
-                    label="Password"
-                    name="password"
-                    placeholder="Password"
-                    type="password"
-                  />
-                  <Button
-                    type="submit"
-                    isLoading={isSubmitting}
-                    width="100%"
-                    mb={4}
-                  >
-                    Login
-                  </Button>
-                </Form>
-              )}
-            </Formik>
-            <Button
-              width="90%"
-              colorScheme="blue"
-              onClick={() => {
-                const googleAuthProvider =
-                  new firebase.auth.GoogleAuthProvider();
-                firebase.auth().signInWithPopup(googleAuthProvider);
-              }}
-            >
-              Login with Google
-            </Button>
-          </VStack>
-        </Grid>
-      </Box>
-    </FirebaseAuthProvider>
+    <Router>
+      <FirebaseAuthProvider firebase={firebase} {...firebaseConfig}>
+        <Box textAlign="center" fontSize="xl">
+          <IfFirebaseUnAuthed>
+            {() => <Redirect to="/login" />}
+          </IfFirebaseUnAuthed>
+          <Switch>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/register">
+              <Register />
+            </Route>
+            <Route path="/">
+              <IfFirebaseAuthed>
+                {({ user }) => <Home user={user} />}
+              </IfFirebaseAuthed>
+            </Route>
+          </Switch>
+        </Box>
+      </FirebaseAuthProvider>
+    </Router>
   </ChakraProvider>
 );
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -11,21 +11,24 @@ import {
 type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
   label: string;
   name: string;
+  validate: (value: any | any[]) => undefined | string | Promise<any>;
 };
 
 export const InputField: React.FC<InputFieldProps> = ({
   label,
   size: _,
   required,
+  validate,
   ...props
 }) => {
-  const [field, { error }] = useField(props);
+  const [field, { error, touched }] = useField({ ...props, validate });
+  const hasError = !!(error && touched && field.value.length > 0);
   return (
     <Box h="100px" mb={2}>
-      <FormControl isInvalid={!!error} isRequired={required}>
+      <FormControl isInvalid={hasError} isRequired={required}>
         <FormLabel htmlFor={field.name}>{label}</FormLabel>
         <Input {...field} {...props} id={field.name} />
-        {error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
+        {hasError ? <FormErrorMessage>{error}</FormErrorMessage> : null}
       </FormControl>
     </Box>
   );

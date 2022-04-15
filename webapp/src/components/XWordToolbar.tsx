8783083d716177a32a60e9583564b07c1bd3ceeb
Nikita Gamolsky <nikita.gamolsky@gmail.com>
2022-04-15 14:48:59 -0700

    bold font in header

--- webapp/src/components/XWordToolbar.tsx
+++ webapp/src/components/XWordToolbar.tsx
@@ -12,7 +12,7 @@ export const XWordToolbar: React.FC<{ belowToolbarContent?: ReactNode }> = ({
       <div className="flex flex-col w-full ">
         <div className="z-10 flex w-full p-1 shadow-sm shadow-black dark:shadow-white ">
           <Link to="/">
-            <p className="mx-4 my-2 rounded-lg flex-0 font-xl dark:active:bg-slate-500 dark:active:bg-opacity-95 active:bg-slate-200 active:bg-opacity-95">
+            <p className="mx-4 my-2 font-bold rounded-lg flex-0 dark:active:bg-slate-500 dark:active:bg-opacity-95 active:bg-slate-200 active:bg-opacity-95">
               {APP_NAME}
             </p>
           </Link>

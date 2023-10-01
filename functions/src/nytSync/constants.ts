c85b0acf03e3f57307cadd89280fc1bbcaed1720
Nikita Gamolsky <nikita.gamolsky@gmail.com>
2023-10-01 09:22:16 -0700

    fix constant url in puzzle sync

--- functions/src/nytSync/constants.ts
+++ functions/src/nytSync/constants.ts
@@ -1,5 +1,5 @@
 export const LATEST_PUZZLE_URL =
-  "https://nyt-games-prd.appspot.com/svc/crosswords/v3/36569100/puzzles.json?sort_order=desc&sort_by=print_date";
+  "https://www.nytimes.com/svc/crosswords/v3/79104332/puzzles.json?sort_order=desc&sort_by=print_date";
 
 export const LATEST_PUZZLE_DATA_BASE_URL =
   "https://www.nytimes.com/svc/crosswords/v6/puzzle/";
